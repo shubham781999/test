c2711762b1d9b161507dd61f130afe180793ddd2
shubham781999 <138647879+shubham781999@users.noreply.github.com>
2026-06-21 18:12:34 +0530

    Update response message in app.jsjdje

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Jenkins Pipeline");
+  res.send("Hello from Jenkins Pipeline test");
 });
 
 app.listen(3000, () => {
