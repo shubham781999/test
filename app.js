ee574b3407efa2c8ebe241033120ddf1a91306d8
root <root@ip-172-31-0-15.ec2.internal>
2026-06-21 11:42:33 +0000

    fjej

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req,res)=>{
-  res.send("Node App Running");
+app.get("/", (req, res) => {
+  res.send("Hello from Jenkins Pipeline");
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
